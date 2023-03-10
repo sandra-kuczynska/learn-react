4a5b31130dd8d9f35a58c9bc4d909e94b34bfb5f
Sandra Kuczynska <sandra.kuczynska@xebia.com>
2023-03-10 12:11:02 +0100

    Initial nav and table

--- src/App.js
+++ src/App.js
@@ -1,53 +1,72 @@
-import i18next from 'i18next';
-import './App.css';
-import { useTranslation, initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import i18next from "i18next";
+import "./App.css";
+import "normalize.css";
+import { useTranslation, initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 const lngs = {
-    en: { nativeName: 'English'},
-    pl: { nativeName: 'Polski'}
+  en: { nativeName: "English" },
+  pl: { nativeName: "Polski" },
 };
 
 i18next
-    .use(initReactI18next)
-    .use(LanguageDetector)
-    .init({
-        debug: true,
-        fallbackLng: 'en',
-        lng: 'en',
-        resources: {
-            en: {
-                translation: {
-                    invoice: 'invoice'
-                }
-            },
-            pl: {
-                translation: {
-                    invoice: 'faktura'
-                }
-            }
-        }
-
-    });
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    debug: true,
+    fallbackLng: "en",
+    lng: "en",
+    resources: {
+      en: {
+        translation: {
+          invoice: "invoice",
+        },
+      },
+      pl: {
+        translation: {
+          invoice: "faktura",
+        },
+      },
+    },
+  });
 
 const App = () => {
-    const { t, i18n } = useTranslation()
+  const { t, i18n } = useTranslation();
 
-    return (
-        <nav>
-            <ul>
-                <li><a href="#">{t('invoice')}</a></li>
-                <li><a href="#">Add new invoice</a></li>
-                <div>
-                    {Object.keys(lngs).map((lng) => (
-                        <button type="submit" key={lng} onClick={() => i18n.changeLanguage(lng)} disabled={i18n.resolvedLanguage === lng}>{lngs[lng.nativeName]}</button>
-                    ))}
-                </div>
-                <button onClick={() => i18n.changeLanguage('pl')}>pl</button>
-                <button onClick={() => i18n.changeLanguage('en')}>en</button>
-            </ul>
-        </nav>
-    );
+  return (
+    <div>
+      <nav class="navbar">
+        <ul>
+          <li>
+            <a class="link" href="#">
+              {t("invoice")}
+            </a>
+          </li>
+          <li>
+            <a class="link" href="#">
+              Add new invoice
+            </a>
+          </li>
+          <div>
+            {Object.keys(lngs).map((lng) => (
+              <button
+                type="submit"
+                key={lng}
+                onClick={() => i18n.changeLanguage(lng)}
+                disabled={i18n.resolvedLanguage === lng}
+              >
+                {lngs[lng.nativeName]}
+              </button>
+            ))}
+          </div>
+          <button onClick={() => i18n.changeLanguage("pl")}>pl</button>
+          <button onClick={() => i18n.changeLanguage("en")}>en</button>
+        </ul>
+      </nav>
+
+      <section class="table">table</section>
+    </div>
+  );
 };
 
 export default App;
