0f3e7806911d3d53cf21f506bb59d353a3067c36
Sandra Kuczynska <sandra.kuczynska@xebia.com>
2023-03-17 10:27:19 +0100

    Move navbar to component NavBar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import i18next from "i18next";
 import "./App.css";
 import "normalize.css";
-import { useTranslation, initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
+
+import NavBar from "./components/NavBar/NavBar";
 
 import * as React from "react";
 import Table from "@mui/material/Table";
@@ -24,69 +23,10 @@ const rows = [
   createData("05/22", "02/02/2022", "02/02/2023", 9000, "icons"),
 ];
 
-const lngs = {
-  en: { nativeName: "English" },
-  pl: { nativeName: "Polski" },
-};
-
-i18next
-  .use(initReactI18next)
-  .use(LanguageDetector)
-  .init({
-    debug: true,
-    fallbackLng: "en",
-    lng: "en",
-    resources: {
-      en: {
-        translation: {
-          invoices: "INVOICES",
-        },
-      },
-      pl: {
-        translation: {
-          invoices: "FAKTURY",
-        },
-      },
-    },
-  });
-
 const App = () => {
-  const { t, i18n } = useTranslation();
-
   return (
     <div class="container">
-      <nav class="navbar">
-        <ul class="nav-ul" id="nav-ul">
-          <li>
-            <a class="link" href="#">
-              {t("invoices")}
-            </a>
-          </li>
-          <li>
-            <a class="link" href="#">
-              EDIT AN INVOICE
-            </a>
-          </li>
-          <li>
-            <a class="link" href="#">
-              ADD NEW INVOICE
-            </a>
-          </li>
-          <div>
-            {Object.keys(lngs).map((lng) => (
-              <button
-                type="submit"
-                key={lng}
-                onClick={() => i18n.changeLanguage(lng)}
-                disabled={i18n.resolvedLanguage === lng}
-              >
-                {lngs[lng].nativeName}
-              </button>
-            ))}
-          </div>
-        </ul>
-      </nav>
-
+      <NavBar />
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,67 @@
+import "./NavBar.module.css";
+import i18next from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+const lngs = {
+  en: { nativeName: "English" },
+  pl: { nativeName: "Polski" },
+};
+
+i18next
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    debug: true,
+    fallbackLng: "en",
+    lng: "en",
+    resources: {
+      en: {
+        translation: {
+          invoices: "INVOICES",
+        },
+      },
+      pl: {
+        translation: {
+          invoices: "FAKTURY",
+        },
+      },
+    },
+  });
+
+const NavBar = () => {
+  const { t, i18n } = useTranslation();
+
+  return (
+    <nav class="navbar">
+      <button class="hamburger" id="hamburger">
+        <i class=""></i>
+      </button>
+      <ul class="nav-ul" id="nav-ul">
+        <li>
+          <a href="#">{t("invoices")}</a>
+        </li>
+        <li>
+          <a href="#">EDIT AN INVOICE</a>
+        </li>
+        <li>
+          <a href="#">ADD NEW INVOICE</a>
+        </li>
+        <div>
+          {Object.keys(lngs).map((lng) => (
+            <button
+              type="submit"
+              key={lng}
+              onClick={() => i18n.changeLanguage(lng)}
+              disabled={i18n.resolvedLanguage === lng}
+            >
+              {lngs[lng].nativeName}
+            </button>
+          ))}
+        </div>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
