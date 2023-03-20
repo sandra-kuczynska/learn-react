1435ed179cc241cf24c36f54fdc2bdcd93c1ec4b
Sandra Kuczynska <sandra.kuczynska@xebia.com>
2023-03-20 09:29:28 +0100

    Add Navigation, MobileNavigation

---
+++ src/components/NavBar/MobileNavigation.js
@@ -0,0 +1,12 @@
+import NavLinks from "./NavLinks";
+import "./NavBar.module.css";
+
+const MobileNavigation = () => {
+  return (
+    <nav class="MobileNavigation">
+      <NavLinks />
+    </nav>
+  );
+};
+
+export default MobileNavigation;
---
+++ src/components/NavBar/Navigation.js
@@ -0,0 +1,12 @@
+import NavLinks from "./NavLinks";
+import "./NavBar.module.css";
+
+const Navigation = () => {
+  return (
+    <nav class="Navigation">
+      <NavLinks />
+    </nav>
+  );
+};
+
+export default Navigation;
