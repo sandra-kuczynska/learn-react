2d0fdf3b0d9179ac6a7d7d6eb55cff8af40ae908
Sandra Kuczynska <sandra.kuczynska@xebia.com>
2023-03-22 10:55:24 +0100

    BrowerRouter. Move NotFoundPage to components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
-import NotFoundPage from "./NotFoundPage.jsx";
+import NotFoundPage from "./components/NotFoundPage/NotFoundPage.jsx";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import TableInvoices from "./components/TableInvoices/TableInvoices.js";
@@ -8,18 +8,16 @@ import Invoice from "./pages/invoice.js";
 
 const App = () => {
   return (
-    <div className="container">
+    <>
       <NavBar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<TableInvoices />} />
-          <Route path="/invoices/add" element={<Invoice />} />
+      <Routes>
+        <Route path="/" element={<TableInvoices />} />
+        <Route path="/invoices/add" element={<Invoice />} />
 
-          {/* 404 rounte */}
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+        {/* 404 rounte */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
   );
 };
 
