bb25eedf12824669a44b81178f6637e48ced1d64
Sandra Kuczynska <sandra.kuczynska@xebia.com>
2023-03-31 16:24:50 +0200

    Refactor routes

--- src/App.js
+++ src/App.js
@@ -6,16 +6,42 @@ import NavBar from "./components/NavBar/NavBar";
 import TableInvoices from "./components/TableInvoices/TableInvoices.js";
 import Invoice from "./pages/invoice.js";
 
+export const routingConfig = {
+  main: {
+    path: "/",
+    element: <TableInvoices />,
+    isInMyMenu: true,
+  },
+  invoices: {
+    path: "/invoices/add",
+    element: <Invoice />,
+    isInMyMenu: true,
+  },
+  notFound: {
+    path: "*",
+    element: <NotFoundPage />,
+  },
+};
+
 const App = () => {
   return (
     <>
       <NavBar />
       <Routes>
-        <Route path="/" element={<TableInvoices />} />
-        <Route path="invoices/add" element={<Invoice />} />
+        <Route
+          path={routingConfig.main.path}
+          element={routingConfig.main.element}
+        />
+        <Route
+          path={routingConfig.invoices.path}
+          element={routingConfig.invoices.element}
+        />
 
         {/* 404 route */}
-        <Route path="*" element={<NotFoundPage />} />
+        <Route
+          path={routingConfig.notFound.path}
+          element={routingConfig.notFound.element}
+        />
       </Routes>
     </>
   );
