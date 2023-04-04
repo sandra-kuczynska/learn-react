249183beac8e741036064ab162aae1cee2bebbd8
Sandra Kuczynska <sandra.kuczynska@xebia.com>
2023-04-04 13:59:37 +0200

    Grid - add colors, add grid to No and Dates

--- src/pages/invoice.js
+++ src/pages/invoice.js
@@ -15,41 +15,44 @@ const Invoice = () => {
   return (
     <div className={styles.grid}>
       <div className={styles.noDates}>
-        <form
-          className="grid grid__NoAndDates"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <label>No.</label>
-          <input
-            className="grid grid__input-no"
-            defaultValue="20/04/23"
-            {...register("inputNo", { required: true })}
-          />
+        <form className="grid__NoAndDates" onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <label>No.</label>
+            <input
+              className="grid__input-no"
+              defaultValue="20/04/23"
+              {...register("inputNo", { required: true })}
+            />
+          </div>
 
-          <label>Created date</label>
-          <input
-            className="grid grid__date-from"
-            defaultValue="20/04/2023"
-            {...register("date-from", { required: true })}
-          />
+          <div>
+            <label>Created date</label>
+            <input
+              className="grid__date-from"
+              defaultValue="20/04/2023"
+              {...register("date-from", { required: true })}
+            />
 
-          <label>Valid until date</label>
-          <input
-            className="grid grid__date-to"
-            defaultValue="20/10/2023"
-            {...register("date-from", { required: true })}
-          />
+            <label>Valid until date</label>
+            <input
+              className="grid__date-to"
+              defaultValue="20/10/2023"
+              {...register("date-from", { required: true })}
+            />
 
-          {errors.inputNo && <span>This field is required</span>}
-
-          <input type="submit" />
+            {errors.inputNo && <span>This field is required</span>}
+          </div>
         </form>
       </div>
-      <div className={styles.saveCancel}>Cancel Save</div>
+
+      <div className={styles.saveCancel}>
+        Cancel
+        <input type="submit" value="Save" />
+      </div>
       <div className={styles.recipient}>recipient</div>
       <div className={styles.sender}>sender</div>
       <div className={styles.info}>info</div>
-      <div className={styles.addItem}>add-item</div>
+      <div className={styles.addItem}>add</div>
     </div>
   );
 };
