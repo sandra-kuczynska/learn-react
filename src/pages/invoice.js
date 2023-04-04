3eec3752486402a6f99cf839fbfcc673a25277bf
Sandra Kuczynska <sandra.kuczynska@xebia.com>
2023-04-04 12:57:24 +0200

    Add initial Invoice's react-hook-form

--- src/pages/invoice.js
+++ src/pages/invoice.js
@@ -1,9 +1,50 @@
 import styles from "./invoice.module.css";
+import { useForm } from "react-hook-form";
 
 const Invoice = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  console.log(watch());
+
   return (
     <div className={styles.grid}>
-      <div className={styles.noDates}>No. dates</div>
+      <div className={styles.noDates}>
+        <form
+          className="grid grid__NoAndDates"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <label>No.</label>
+          <input
+            className="grid grid__input-no"
+            defaultValue="20/04/23"
+            {...register("inputNo", { required: true })}
+          />
+
+          <label>Created date</label>
+          <input
+            className="grid grid__date-from"
+            defaultValue="20/04/2023"
+            {...register("date-from", { required: true })}
+          />
+
+          <label>Valid until date</label>
+          <input
+            className="grid grid__date-from"
+            defaultValue="20/10/2023"
+            {...register("date-from", { required: true })}
+          />
+
+          {errors.inputNo && <span>This field is required</span>}
+
+          <input type="submit" />
+        </form>
+      </div>
       <div className={styles.saveCancel}>Cancel Save</div>
       <div className={styles.recipient}>recipient</div>
       <div className={styles.sender}>sender</div>
